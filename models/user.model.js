ecc78a9dc78e03a7c67b37c1eac0b334cc662708
davidheweb <davidhermosilladuro@gmail.com>
2022-10-04 14:49:18 +0200

    Adding user input validation

--- models/user.model.js
+++ models/user.model.js
@@ -18,6 +18,15 @@ class User {
     return db.getDb().collection("users").findOne({ email: this.email });
   }
 
+  async existsAlready() {
+    const existingUser = await this.getUserWithSameEmail();
+    if (existingUser) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   async signup() {
     const hashedPassword = await bcrypt.hash(this.password, 12);
 
@@ -30,7 +39,7 @@ class User {
   }
 
   hasMatchingPassword(hashedPassword) {
-    return bcrypt.compare(this.password, hashedPassword)
+    return bcrypt.compare(this.password, hashedPassword);
   }
 }
 
